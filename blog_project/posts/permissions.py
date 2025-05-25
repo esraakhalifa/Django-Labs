12815fb283e71ddabbf2415583ac3e3a102dfb2b
Esraa Khalifa <esraakhalifa5@gmail.com>
2025-05-25 17:32:23 +0300

    feat: separate permission classes of the post listing and the post editing apis, making the listing available to the public but the editing as is, available only to the post author

--- blog_project/posts/permissions.py
+++ blog_project/posts/permissions.py
@@ -2,14 +2,10 @@
 
 from rest_framework import permissions
 
-class IsOwnerOrReadOnly(permissions.BasePermission):
+class IsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        # Safe methods are allowed for everyone (if authenticated)
+
         if request.method in permissions.SAFE_METHODS:
             return True
-        # Write permissions only for the post owner
-        return obj.author == request.user
 
-    def has_permission(self, request, view):
-        # User must be authenticated for any action
-        return request.user and request.user.is_authenticated
+        return obj.author == request.user
--- blog_project/posts/urls.py
+++ blog_project/posts/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
-from .views import PostDetail, PostList,PostCreate, RegisterView, CustomTokenObtainPairView
+from .views import PostRetrieveView, PostUpdateDeleteView, PostList,PostCreate, RegisterView, CustomTokenObtainPairView
 from rest_framework_simplejwt.views import  TokenRefreshView
 
 
 urlpatterns = [ 
     path('', PostList.as_view(), name='post_list'),
     path('create/', PostCreate.as_view(), name='post_create'),
-    path('<int:pk>/', PostDetail.as_view(), name='post_detail'),
+    path('<int:pk>/', PostRetrieveView.as_view(), name='post-detail'),
+    path('<int:pk>/edit/', PostUpdateDeleteView.as_view(), name='post-update-delete'),
     path('author/register/', RegisterView.as_view(), name='register'),
     path('author/login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),  # login, returns access & refresh tokens
     path('author/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
